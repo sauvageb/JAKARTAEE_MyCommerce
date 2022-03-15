b8feded60b64a72c66e73c5c2cf3694f17c2493e
sauvageb <sauvageboris.pro@gmail.com>
2022-03-15 11:52:09 +0100

    feat(audit): create audit filter to display incoming http messages

---
+++ src/main/java/com/mycommerce/project/filter/IncomingRequestsAuditFilter.java
@@ -0,0 +1,40 @@
+package com.mycommerce.project.filter;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@WebFilter("/*")
+public class IncomingRequestsAuditFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        System.out.println("###### " + httpServletRequest.getProtocol() + " " + httpServletRequest.getRequestURI() + " ###### ###### \n");
+        System.out.print("###### ###### Headers ");
+        Map<String, String> headers = Collections.list(httpServletRequest.getHeaderNames())
+                .stream()
+                .collect(Collectors.toMap(h -> h, httpServletRequest::getHeader));
+        headers.entrySet().forEach(h -> System.out.println(h));
+
+        System.out.print("###### ###### Body ");
+        Map<String, String[]> parameters = httpServletRequest.getParameterMap();
+        if (parameters.isEmpty()) {
+            System.out.println("\n");
+        }
+        parameters.entrySet()
+                .stream()
+                .forEach(p -> {
+                    System.out.println(p.getKey() + " -> " + Arrays.toString(p.getValue()));
+                });
+        System.out.print("###### ###### ###### ###### ######\n");
+        chain.doFilter(request, response);
+    }
+}
